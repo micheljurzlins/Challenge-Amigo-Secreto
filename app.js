22efdd369dc81f5b214f21fea7904b8eaec5c971
Michel Jurzlins <j_linares@outlook.com>
2025-07-29 18:38:00 -0600

    Capturar el valor del campo de entrada

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigos() {
+    //Capturar el valor del campo de entrada
+    const input = document.getElementById("amigo");
+}
+
